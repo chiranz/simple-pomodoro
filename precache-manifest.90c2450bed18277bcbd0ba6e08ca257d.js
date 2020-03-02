6f3b91fbcbe0ca63e89b126637d5ef395c3cff9d
chiranz <chiranpoudyal@gmail.com>
2020-03-02 14:26:40 +0530

    Updates

---
+++ precache-manifest.90c2450bed18277bcbd0ba6e08ca257d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d55753ea5817a05b587d9ef6c2183da",
+    "url": "/simple-pomodoro/index.html"
+  },
+  {
+    "revision": "1a6919a3af6123008d58",
+    "url": "/simple-pomodoro/static/css/main.32855770.chunk.css"
+  },
+  {
+    "revision": "4ef601f5103dc59a6309",
+    "url": "/simple-pomodoro/static/js/2.0ebbab0e.chunk.js"
+  },
+  {
+    "revision": "9fe7282b7eac65c14126621a194fec29",
+    "url": "/simple-pomodoro/static/js/2.0ebbab0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6919a3af6123008d58",
+    "url": "/simple-pomodoro/static/js/main.74ca685d.chunk.js"
+  },
+  {
+    "revision": "9d6ef8ce794aaf276e28",
+    "url": "/simple-pomodoro/static/js/runtime-main.bf16148b.js"
+  }
+]);
